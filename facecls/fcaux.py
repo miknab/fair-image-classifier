6b68ce1edc53b0bbb654786ba8e62d969a33fa33
Mischa Knabenhans <mischak@physik.uzh.ch>
2024-08-07 23:35:25 +0200

    Added some documentation and removed unused code
    Added documentation for set_seed and pxlstring2pxlvec functions.
    In the latter function, some unused code was removed in addition.

--- facecls/fcaux.py
+++ facecls/fcaux.py
@@ -10,7 +10,21 @@ import tensorflow as tf
 import random
 
 
-def set_seed(seed=42):
+def set_seed(seed: int = 42) -> int:
+    """
+    Sets all relevant seeds and returns the seed value.
+
+    Sets seeds by defining environment variables,
+    tensorflow, numpy and random seeds. The value
+    of the seed is returned so it can be used in 
+    other places if required ensuring consistency.
+
+    Keyword arguments:
+    seed -- value of the seed to be used (default: 42)
+
+    Returns:
+    seed -- value of the seed to be used (default: 42)
+    """
     os.environ['PYTHONHASHSEED'] = str(seed)
     os.environ['TF_DETERMINISTIC_OPS'] = '1'
     tf.random.set_seed(seed)
@@ -18,20 +32,42 @@ def set_seed(seed=42):
     random.seed(seed)
     return seed
 
-def pxlstring2pxlvec(df: pd.DataFrame, idx: int) -> np.ndarray:
+def pxlstring2pxlvec(df: pd.DataFrame, 
+                     idx: int, 
+                     colname: str = "pixels",
+                     sep: str = " "
+                    ) -> np.ndarray:
     """
-    Takes in a string of space-separated integers and converts it into
-    an 1D array of integers corresponding to the vectorized array of
-    pixel values.
+    Converts a string of pixel values to a vector of pixel values.
+    
+    The pixel string at row index "idx" in the "colname" column of
+    "df" is converted into a 1D array of integers. This corresponds
+    to a vectorized array of pixel values.
+
+    Arguments:
+    df  -- dataframe containing the pixel value strings
+    idx -- row index of df to be selected
+
+    Keyword arguments:
+    colname -- name of column in df containing the pixel value strings
+               (default: "pixels")
+    sep     -- separator used between pixel values in pixel string 
+               (default: " ")
+    
+    Returns:
+    img -- vector of pixel values
     """
-    pxl_str = df["pixels"][idx]
-    pxl_list = pxl_str.split(" ")
-    n_pxls = len(pxl_list)
+    # Check if colname is a valid column name
+    if colname not in df.columns:
+        raise ValueError("colname must be an existing column name of "
+                         + "the pd.DataFrame df."
+                        )
 
-    img_dim = np.sqrt(n_pxls)
-    assert float(int(img_dim)) == img_dim
-    img_dim = int(img_dim)
-
+    # Extract the pixel string at row index idx (that's the only
+    # pixel string considered in this execution of the pxlstring2pxlvec
+    # function).
+    pxl_str = df[colname][idx]
+    pxl_list = pxl_str.split(sep)
     img = np.array(pxl_list, dtype=int)
     return img
     
